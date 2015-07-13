aa9b88c0d755b9cc0ec002f88ed30e549938197b
Matthias Lamoureux <letatas@gmail.com>
2015-07-13 22:38:58 +0200

    ASCII-arting the output...

--- console/yafacwesConsole/main.m
+++ console/yafacwesConsole/main.m
@@ -13,14 +13,19 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        [@"******************************" stdoutPrintln];
-        [@"*   Y  A  F  A  C  W  E  S   *" stdoutPrintln];
-        [@"******************************" stdoutPrintln];
+        [@"****************************************************" stdoutPrintln];
+        [@"*    __  _____   _______  ______      __________   *" stdoutPrintln];
+        [@"*    \\ \\/ / _ | / __/ _ |/ ___/ | /| / / __/ __/   *" stdoutPrintln];
+        [@"*     \\  / __ |/ _// __ / /__ | |/ |/ / _/_\\ \\     *" stdoutPrintln];
+        [@"*     /_/_/ |_/_/ /_/ |_\\___/ |__/|__/___/___/     *" stdoutPrintln];
+        [@"*                                                  *" stdoutPrintln];
+        [@"****************************************************" stdoutPrintln];
+        [@"" stdoutPrintln];
         
         CWSCore * core = [[CWSCore alloc] init];
         core.name = @"MIW Core";
         
-        [core.name stdoutPrintln];        
+        [core.name stdoutPrintln];
     }
     return 0;
 }
