73b0c7fb0ab0c6d006a9b424432283047d653240
Matthias Lamoureux <letatas@gmail.com>
2015-07-13 22:24:00 +0200

    Added Core class

---
+++ sources/CWSCore.h
@@ -0,0 +1,13 @@
+//
+//  CWSCore.h
+//  yafacwesConsole
+//
+//  Created by Matthias Lamoureux on 13/07/2015.
+//  Copyright (c) 2015 pinguzaph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWSCore : NSObject
+
+@end
---
+++ sources/CWSCore.m
@@ -0,0 +1,13 @@
+//
+//  CWSCore.m
+//  yafacwesConsole
+//
+//  Created by Matthias Lamoureux on 13/07/2015.
+//  Copyright (c) 2015 pinguzaph. All rights reserved.
+//
+
+#import "CWSCore.h"
+
+@implementation CWSCore
+
+@end
