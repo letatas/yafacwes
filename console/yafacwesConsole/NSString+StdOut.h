3cfb552bb21c07c5088b501883553f89931ca184
Matthias Lamoureux <letatas@gmail.com>
2015-07-13 21:49:36 +0200

    Added stdout category to NSString

---
+++ console/yafacwesConsole/NSString+StdOut.h
@@ -0,0 +1,16 @@
+//
+//  NSString+StdOut.h
+//  yafacwesConsole
+//
+//  Created by Matthias Lamoureux on 13/07/2015.
+//  Copyright (c) 2015 pinguzaph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (StdOut)
+
+- (void) stdoutPrint;
+- (void) stdoutPrintln;
+
+@end
--- console/yafacwesConsole/main.m
+++ console/yafacwesConsole/main.m
@@ -11,7 +11,9 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        [@"******************************" stdoutPrintln];
+        [@"*   Y  A  F  A  C  W  E  S   *" stdoutPrintln];
+        [@"******************************" stdoutPrintln];
     }
     return 0;
 }
