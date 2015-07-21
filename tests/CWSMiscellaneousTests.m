f1020bea4f09104e6b0f2186b6021d35616100c3
Matthias Lamoureux <letatas@gmail.com>
2015-07-21 10:08:54 +0200

    Added little test to check @encode(typeof(...)) for gnustep

---
+++ tests/CWSMiscellaneousTests.m
@@ -0,0 +1,48 @@
+//
+//  CWSMiscellaneousTests.m
+//  yafacwesConsole
+//
+//  Created by Matthias Lamoureux on 21/07/2015.
+//  Copyright (c) 2015 pinguzaph. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface CWSMiscellaneousTests : XCTestCase
+
+@end
+
+@implementation CWSMiscellaneousTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testEnoceTypeOf {
+    // Arrange
+    NSUInteger count = 3;
+    NSArray * tab = @[@1,@2,@3];
+    
+    // Act
+    char * encodeUInt = @encode(typeof(count));
+    char * encodePropertyCall = @encode(typeof(tab.count));
+    char * encodeMethodCall = @encode(typeof([tab count]));
+    
+    BOOL encodeUIntOk = encodeUInt[0] == 'Q';
+    BOOL encodePropertyCallOk = encodePropertyCall[0] == 'Q';
+    BOOL encodeMethodCallOk = encodeMethodCall[0] == 'Q';
+    
+    // Assert
+    XCTAssert(encodeUIntOk);
+    XCTAssert(encodePropertyCallOk);
+    XCTAssert(encodeMethodCallOk);
+}
+
+@end
