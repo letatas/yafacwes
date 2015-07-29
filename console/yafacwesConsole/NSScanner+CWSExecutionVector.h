ebf10721ae51673e31c216513a21580188eb4d04
Matthias Lamoureux <letatas@gmail.com>
2015-07-29 15:00:29 +0200

    Added files to test scheme

---
+++ console/yafacwesConsole/NSScanner+CWSExecutionVector.h
@@ -0,0 +1,17 @@
+//
+//  NSScanner+CWSExecutionVector.h
+//  yafacwesConsole
+//
+//  Created by Damien Brossard on 22/07/2015.
+//  Copyright (c) 2015 pinguzaph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CWSExecutionVector.h"
+
+@interface NSScanner(CWSExecutionVector)
+
+- (BOOL) scanExecutionVector: (CWSExecutionVector **) executionVector;
+- (BOOL) scanExecutionVectorInArray: (NSMutableArray *) array;
+
+@end
