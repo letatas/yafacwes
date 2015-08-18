ba2c5721da201634abb3ee0a339eefb02a0c2ddf
Damien Brossard <brossard.damien@gmail.com>
2015-08-18 11:59:49 +0200

    (todo #36) ADD knows how to add two positions...

--- sources/CWSInstructionADD.m
+++ sources/CWSInstructionADD.m
@@ -10,10 +10,30 @@
 
 @implementation CWSInstructionADD
 
+- (id) computeResultWithA:(id) paramA andB:(id) paramB {
+    if ([paramA respondsToSelector:@selector(positionValue)] && [paramB respondsToSelector:@selector(positionValue)]) {
+        CWSPosition posA = [paramA positionValue];
+        CWSPosition posB = [paramB positionValue];
+        
+        return [NSValue valueWithPosition:CWSPositionAdd(posA, posB)];
+    }
+    return nil;
+}
+
 - (BOOL) executeForCoreState:(CWSCoreState *) aCoreState {
     CWSExecutionVector * ev = aCoreState.nextExecutionVector;
       
     BOOL result = NO;
+    if ([ev stackSize] > 1) {
+        CWSParametrizedInstruction * operandA = [ev popOnStack];
+        CWSParametrizedInstruction * operandB = [ev popOnStack];
+        
+        id param = [self computeResultWithA:operandA.parameter andB:operandB.parameter];
+        
+        operandA.parameter = param;
+        [ev pushOnStack:operandA];    
+        result = YES;
+    }
     
     if (result) {
         [ev move];
